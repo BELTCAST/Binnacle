46ba2fa86188a1d23671ddb7748aa09436aeb3e3
Nicolas Beltran <nicolas.bel.cas@gmail.com>
2023-09-05 14:53:09 -0500

    REPL concept added

---
+++ Conceptos/REPL.py
@@ -0,0 +1,32 @@
+# REPL
+
+# Que es el REPL? 
+
+# "REPL" es una abreviatura de "Read-Eval-Print Loop" en inglés, que se traduce como "Bucle de Lectura-Evaluación-Impresión". Es un entorno de programación interactivo que permite a los desarrolladores escribir y ejecutar código de forma incremental y ver los resultados de inmediato. Los REPLs son comunes en varios lenguajes de programación y se utilizan principalmente con lenguajes interpretados como Python, Ruby, JavaScript, y otros.
+
+# Un REPL generalmente consta de tres componentes principales:
+
+# Lectura (Read): El REPL lee las instrucciones o comandos que el usuario ingresa. Esto puede ser una única línea de código o una serie de instrucciones.
+
+# Evaluación (Eval): Luego, el REPL interpreta y evalúa el código ingresado por el usuario. Esto significa que ejecuta el código y calcula los resultados.
+
+# Impresión (Print): Después de evaluar el código, el REPL muestra el resultado en la pantalla para que el usuario lo vea. Esto puede ser el resultado de una operación matemática, el valor de una variable, o cualquier otro resultado que genere el código.
+
+# Bucle (Loop): Una vez que se ha completado el ciclo de lectura, evaluación e impresión, el REPL vuelve al estado de lectura para esperar la siguiente entrada del usuario. Este ciclo se repite continuamente, permitiendo una programación interactiva y rápida.
+
+# Los REPLs son útiles para probar fragmentos de código, experimentar con nuevas ideas, depurar problemas en el código y aprender un lenguaje de programación. Son especialmente populares en el desarrollo y prototipado rápido de software, ya que permiten a los desarrolladores interactuar directamente con el código sin necesidad de escribir un programa completo.
+
+# Por ejemplo, en Python, puedes abrir un REPL ejecutando el comando python o python3 en la línea de comandos, lo que te permite ingresar y ejecutar código Python de forma interactiva.
+
+# Ejemplo: 
+
+# En consola hacemos la siguiente operacion: 
+
+# >>> 2 + 2 
+
+# Nos va a retornar un output:
+
+# 4 
+
+
+
